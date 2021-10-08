d9429c99c70baeb7d813128b07c799b511b44a91
Rodrigo-MimuraM <>
2021-10-08 18:18:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import keep_alive
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from music import music_cog
+
+bot = commands.Bot(command_prefix='-')
+
+#register the class with the bot
+
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+keep_alive.keep_alive()
+bot.run(os.environ['token'])
